ad2496e349b5f1e2f32cbbf17e66d6c45310f234
Akshita Gupta <akshitadvlp@gmail.com>
2018-01-14 14:36:50 +0530

    Adding audio files path

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 #Path for audio files here
+# We define where does all the audio files reside
+wav_fd = 'trim'
+
+# These are the folders where various features will be extracted
+fe_cqt_fd              = 'Fe/cqt'
+fe_logmel_kong_fd      = 'Fe/logmel_kong'
+fe_logmel_libd_fd      = 'Fe/logmel_lib_delta'
+fe_mel_fd              = 'Fe/mel'
 
